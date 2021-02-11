3d1f35629570f467a71b2164d7043ee976856bea
Bouquet Vincent <bouquet.vincent704@orange.fr>
2021-02-11 17:58:50 +0100

    1rst

---
+++ database.py
@@ -0,0 +1,28 @@
+from mysql import connector
+
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv()
+
+db = connector.connect(
+    host="localhost",
+    database=getenv('DB_NAME'),
+    user=getenv('DB_USER'),
+    password=getenv('DB_PASS')
+)
+cursor = db.cursor(dictionary=True)
+
+query = "SELECT * FROM films WHERE title LIKE %(title_to_find)s"
+
+cursor.execute(query, {"title_to_find": "%star%"})
+
+# films = cursor.fetchall()
+# print(films[2])
+
+while True:
+    films = cursor.fetchone()
+    if films == None:
+        break
+    else:
+        # print(user)
+        print(films['title'])
---
+++ main.py
@@ -0,0 +1,8 @@
+import tkinter
+
+class research:
+    def __init__(self):
+        self.word_input = ""
+        self.list_film = []
+
+        self.label1 = tkinter.Entry(tkinter.Tk, textvariable=)
